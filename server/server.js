5a6552f94ba9bc557cac7d4b508ed479cb90dd09
shaharcohen1997@gmail.com <shaharcohen1997@gmail.com>
2018-10-10 12:18:52 +0300

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -32,6 +32,22 @@ app.get('/todos',(request, response) => {
     }).catch((err) => {
        response.status(400).send(err);
     })
+});
+app.get('/todos/:id', (request, response) => {
+    const id = request.params.id;
+    if(!ObjectID.isValid(id)){
+       return response.status(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+          return response.status(404).send();
+        }
+        response.send({todo});
+    }).catch((e) => {
+        response.status(400).send();
+    });
+
+    
 });
 app.listen(3000, () => {
     console.log('Started on port 3000');
